8d1d1cd083b234b380bf6e89f629a2402560a85d
Kang Kyu Lee <kangkyu1111@gmail.com>
2014-08-28 18:47:46 -0700

    Design page Layouts

--- app/models/roasting.rb
+++ app/models/roasting.rb
@@ -1,4 +1,4 @@
 class Roasting < ActiveRecord::Base
-  has_attached_file :image, :styles => { :medium => "300x", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  has_attached_file :image, :styles => { :medium => "300x", :thumb => "100x100>" }, :default_url => "default.jpg"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
