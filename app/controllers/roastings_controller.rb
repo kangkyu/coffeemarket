4aeccad43c05fe8b2a21bd4a649b0e8741d16398
Kang Kyu Lee <kangkyu1111@gmail.com>
2014-10-08 16:23:00 -0700

    image not found problem solved

--- app/controllers/roastings_controller.rb
+++ app/controllers/roastings_controller.rb
@@ -1,6 +1,6 @@
 class RoastingsController < ApplicationController
   before_action :set_roasting, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!, except: :index
   before_filter :check_user, only: [:edit, :update, :destroy]
 
   def seller
